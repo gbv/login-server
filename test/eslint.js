6c70db9cc8ca031a1f89bee093e5264d1de032f9
Stefan Peters <Stefan.Peters@gbv.de>
2019-02-08 09:41:58 +0100

    Add first test (ESLint) (#1)

---
+++ test/eslint.js
@@ -0,0 +1,11 @@
+const lint = require("mocha-eslint")
+
+// ESLint as part of the tests
+let paths = [
+  "**/*.js",
+  "!node_modules/**/*.js"
+]
+let options = {
+  contextName: "ESLint"
+}
+lint(paths, options)
