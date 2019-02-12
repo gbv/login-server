437876f1e5598778c97c6e1d2d96364e62df1bde
Stefan Peters <Stefan.Peters@gbv.de>
2019-02-12 09:45:45 +0100

    Adjust WebSocket test

--- test/websocket.js
+++ test/websocket.js
@@ -11,15 +11,21 @@ describe("WebSocket", () => {
       expect(port).to.be.equal(listener.address().port)
       let address = `ws://localhost:${port}/`
       let ws = new WebSocket(address)
+      let responses = 0, expected = 2
       ws.on("message", message => {
+        responses += 1
         let event = JSON.parse(message)
         if (event.type == "providers") {
           // End test when providers were received
           expect(event.data).to.be.an("object")
           expect(event.data.providers).to.be.an("array").with.length(1)
-          done()
+        } else if (event.type == "tokens") {
+          // TODO: Add as soon as WebSockets can be tested with authorization.
         } else {
           expect(event.type).to.be.equal("loggedOut")
+        }
+        if (responses == expected) {
+          done()
         }
       })
       ws.on("open", () => {
