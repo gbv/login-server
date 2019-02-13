9f427c7f7540d5a48be6ecde69dff2ec6af5d6f5
Stefan Peters <Stefan.Peters@gbv.de>
2019-02-13 13:11:56 +0100

    Allow multiple WebSockets for a single session

--- routes/websocket.js
+++ routes/websocket.js
@@ -8,37 +8,41 @@ const events = require("../lib/events")
 module.exports = app => {
 
   app.ws("/", (ws, req) => {
-  // Add sessionID to websockets
-    const sessionID = req.sessionID
-    events.addSocket(sessionID, ws)
+    // Generate a unique identifier for this WebSocket
+    const wsID = utils.uuid()
+    let socket = {
+      sessionID: req.sessionID,
+      ws
+    }
+    events.addSocket(wsID, socket)
 
     if (req.user) {
       // Fire loggedIn event
-      events.userLoggedIn(sessionID, req.user)
+      events.userLoggedInWs(wsID, req.user)
     } else {
       // Fire loggedOut event
-      events.userLoggedOut(sessionID, null)
+      events.userLoggedOutWs(wsID, null)
     }
     ws.on("message", (message) => {
       try {
         message = JSON.parse(message)
         if (message.type === "providers") {
         // Reply with list of providers
-          events.sendEvent(sessionID, "providers", {
+          events.sendEvent(wsID, "providers", {
             providers: utils.prepareProviders()
           })
         }
         if (message.type === "token") {
-          events.sendToken(sessionID)
+          events.sendToken(wsID)
         }
       } catch(error) {
       // Send error event to WebSocket
-        events.error(sessionID, "Message could not be parsed.")
+        events.error(wsID, "Message could not be parsed.")
       }
     })
     ws.on("close", () => {
-    // Remove sessionID from websockets
-      events.removeSocket(sessionID)
+    // Remove wsID from websockets
+      events.removeSocket(wsID)
     })
   })
 
