4c9e6eeb1858bda86d79105c2a913ec9f8f1df21
Stefan Peters <Stefan.Peters@gbv.de>
2019-02-26 10:52:44 +0100

    Adjust MongoStore
    All current sessions need to be removed from the database for this
    change. This change is necessary for #13.

--- utils/mongoStore.js
+++ utils/mongoStore.js
@@ -5,6 +5,9 @@
 const config = require("../config")
 const session = require("express-session")
 const MongoStore = require("connect-mongo")(session)
-const mongoStore = new MongoStore({ url: config.database.url })
+const mongoStore = new MongoStore({
+  url: config.database.url,
+  stringify: false,
+})
 
 module.exports = mongoStore
