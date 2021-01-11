3e0404fc40e4ce561f6e118d1962fa8c9ccb48e2
Stefan Peters <Stefan.Peters@gbv.de>
2021-01-11 14:26:31 +0100

    Update passport-easydb integration (#43)

--- strategies/easydb.js
+++ strategies/easydb.js
@@ -7,7 +7,7 @@ const Strategy = require("passport-easydb")
 module.exports = (options, provider, callback) => new Strategy(options, (req, accessToken, refreshToken, profile, done) => {
   callback(req, null, null, {
     id: profile.id,
-    name: profile.displayName,
+    name: profile._json.user._generated_displayname,
     username: profile.username,
     token: accessToken,
     provider: provider.id
