f345a2e50b6997c9c730f99929704c2a681b1d3c
Stefan Peters <Stefan.Peters@gbv.de>
2021-01-12 12:18:39 +0100

    Add eslint-config-gbv

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,12 @@
 module.exports = {
-  "env": {
-    "es6": true
+  env: {
+    es6: true,
+    node: true,
   },
-  "extends": ["eslint:recommended"],
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaVersion": 2017
+  extends: ["gbv"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2017,
   },
-  "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "no-undef": "off",
-    "no-console": "off"
-  }
+  rules: {},
 }
