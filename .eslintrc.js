d12b0684c55d9be1522c83b286bfe7e4f2a3cbc8
Stefan Peters <Stefan.Peters@gbv.de>
2021-01-12 11:21:31 +0100

    ESLint fix .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
   "env": {
-      "es6": true
+    "es6": true
   },
   "extends": ["eslint:recommended"],
   "parserOptions": {
-      "sourceType": "module",
-      "ecmaVersion": 2017
+    "sourceType": "module",
+    "ecmaVersion": 2017
   },
   "rules": {
-      "indent": [
-          "error",
-          2
-      ],
-      "linebreak-style": [
-          "error",
-          "unix"
-      ],
-      "quotes": [
-          "error",
-          "double"
-      ],
-      "semi": [
-          "error",
-          "never"
-      ],
-      "no-undef": "off",
-      "no-console": "off"
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-undef": "off",
+    "no-console": "off"
   }
 }
