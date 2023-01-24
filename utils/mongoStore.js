2763d8205b8c02cc0ab8576e7f68c5a06e6c0ea1
Stefan Peters <stefandesu@exo.pm>
2023-01-24 07:55:41 +0100

    Update connect-mongo to latest version, adjusting mongoStore util (#83)

--- utils/mongoStore.js
+++ utils/mongoStore.js
@@ -2,24 +2,18 @@
  * Exports a MongoStore instance that is used to persist sessions.
  */
 
-// const config = require("../config")
-const { connection } = require("./db")
-const session = require("express-session")
-const MongoStore = require("connect-mongo")(session)
-const mongoStore = new MongoStore({
-  mongooseConnection: connection,
+const db = require("./db")
+const MongoStore = require("connect-mongo")
+const mongoStore = MongoStore.create({
+  clientPromise: db.connect(true).then(m => m.connection.getClient()),
   stringify: false,
 })
 
-// Workaround for connection failures in the mongoose connection.
-// Without this, the MongoStore will not work even if the mongoose connection is reconnected.
-connection.on("connected", async () => {
-  // handleNewConnectionAsync sometimes throws an error while still doing what it should for us.
-  try {
-    await mongoStore.handleNewConnectionAsync(connection)
-  } catch(error) {
-    // Ignore error
-  }
+// Promisify certain methods on MongoStore instance
+const util = require("util")
+;["length", "clear", "get", "set", "all", "touch", "destroy", "close"].forEach(name => {
+  const method = mongoStore[name]
+  mongoStore[name] = util.promisify(method).bind(mongoStore)
 })
 
 module.exports = mongoStore
